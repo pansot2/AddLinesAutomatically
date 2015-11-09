71a1604213ea5c06d416aac9740e19368383bf35
Panagiotis Sotiropoulos <panossot@uop.gr>
2015-11-09 22:41:20 +0100

    Adding the project.

---
+++ appendFileLines/src/main/java/gr/uop/intermittentfaults/appendfilelines/AddFileLines.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.uop.intermittentfaults.appendfilelines;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Panos
+ */
+public class AddFileLines {
+    public static void addLines(File file, List<Integer> additionalLines, ArrayList<String> paramsToMonitor, String className) throws IOException {
+        List<String> lines = Files.readAllLines(file.toPath());
+        int i=0;
+        for (int line : additionalLines)
+            lines.set(line-1, lines.get(line-1) + " " + "try {JbossAutomatedJavaSeMetrics.metric(\"" + className + "\"," + paramsToMonitor.get(i) + ",\"" + paramsToMonitor.get(i++) + "\",\"intermittentFaultsGroup\");} catch (Exception ex) {ex.printStackTrace();}");
+        Files.write(file.toPath(), lines);
+    }
+}
---
+++ appendFileLines/src/main/java/gr/uop/intermittentfaults/appendfilelines/Main.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.uop.intermittentfaults.appendfilelines;
+
+import gr.uop.intermittent.faults.intermittentfaultstest.Test;
+import gr.uop.intermittent.faults.utils.Cache;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.jboss.metrics.jbossautomatedmetricslibrary.MetricsCache;
+
+/**
+ *
+ * @author Panos
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        
+        File file = new File("F:\\Netbeans\\IntermittentFaults\\IntermittentFaultsTest\\src\\main\\java\\gr\\uop\\intermittent\\faults\\utils\\Store.java");
+        List<Integer> additionalLines = new ArrayList<>();
+        ArrayList<String> paramsToMonitor = new ArrayList<>();
+        additionalLines.add(34);
+        paramsToMonitor.add("value");
+        AddImport.addLines(file, "import org.jboss.metrics.javase.automatedmetricsjavaseapi.JbossAutomatedJavaSeMetrics;");
+        AddFileLines.addLines(file, additionalLines, paramsToMonitor, "CacheStore");
+
+        Runtime.getRuntime().exec("cmd /c start C:\\Users\\Panos\\Documents\\NetBeansProjects\\appendFileLines\\src\\main\\java\\gr\\uop\\intermittentfaults\\appendfilelines\\runCompile.bat");
+        Thread.sleep(10000);
+         
+        for (int i=0; i<10; i++) {
+            MetricsCache cache = Test.test(args);
+
+            if (cache!=null) {
+                System.out.println("Cache : " );
+                cache.printMetricObjects();
+            }
+        }
+        
+        Runtime.getRuntime().exec("cmd /c start C:\\Users\\Panos\\Documents\\NetBeansProjects\\appendFileLines\\src\\main\\java\\gr\\uop\\intermittentfaults\\appendfilelines\\runReset.bat");
+    }
+}
